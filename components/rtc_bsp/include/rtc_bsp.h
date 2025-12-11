b2477736ca12a9da7bcce6059612f69e83f1c34a
杨溢 <121284031@qq.com>
2025-12-11 14:44:19 +0800

    Initial commit

---
+++ components/rtc_bsp/include/rtc_bsp.h
@@ -0,0 +1,5 @@
+#ifndef __RTC_BSP_H_
+#define __RTC_BSP_H_
+
+
+#endif
---
+++ components/rtc_bsp/rtc_bsp.c
@@ -0,0 +1,11 @@
+#include "rtc_bsp.h"
+#include "esp_check.h"
+#include "esp_io_expander_tca9554.h"
+#include "esp_log.h"
+
+static const char *TAG = "RTC_BSP";
+
+esp_err_t rtc_bsp_init(void) {
+
+
+}
